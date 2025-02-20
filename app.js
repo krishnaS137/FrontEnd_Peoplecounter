b1b77e7958e47c62305c200d0afddb58da5fec73
Krishna Sonar <sonarkrishna137@gmail.com>
2025-02-20 18:36:42 +0530

    Changes Made for task in javascript tutorial

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 let myage=19;
 console.log(myage);
+let bonuspoints=50;
+console.log(bonuspoints);
+bonuspoints=bonuspoints+50;
+
+console.log(bonuspoints);
+
+bonuspoints=bonuspoints-75;
+
+console.log(bonuspoints);
+bonuspoints=bonuspoints+
