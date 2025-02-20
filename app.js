0d0ffc3d6f7206353cb9c77cf9936b2495f193e5
Krishna Sonar <sonarkrishna137@gmail.com>
2025-02-20 18:27:50 +0530

    Commit from a remote repository

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let myage=19;
+<<<<<<< HEAD
 console.log(myage);
 let bonuspoints=50;
 console.log(bonuspoints);
@@ -12,3 +13,6 @@ console.log(bonuspoints);
 bonuspoints=bonuspoints+45;
 
 console.log(bonuspoints);
+=======
+console.log(myage);
+>>>>>>> 047e9e9 (Commit from a remote repository)
