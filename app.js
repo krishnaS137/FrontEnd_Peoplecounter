1e819543f808a8e95349bb1ec97948cc6016dfa0
Krishna Sonar <sonarkrishna137@gmail.com>
2025-02-25 12:17:10 +0530

    This is a commit which i forgot to do

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-//Making changes in the js for writing a fucntion
-function logoutnumber(){
-    console.log(42);
+// //Making changes in the js for writing a fucntion
+// function logoutnumber(){
+//     console.log(42);
+// }
+// //calling the function
+// logoutnumber();
+
+// //making a function that sums up the numbers given to it 
+// function sumation(number1,number2,number3){
+//     let sum =number1+number2+number3;
+//     return sum;
+// }
+
+// let number1=100;
+// let number2=200;
+// let number3=300;
+
+// let result = sumation(number1,number2,number3);
+// console.log(result);
+
+//creating a function that increments a given number when called4
+let number=10;
+function increments(){
+    number=number+1;  
 }
-//calling the function
-logoutnumber();
+increments();
+increments();
+increments();
+console.log(number);
